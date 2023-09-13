59f7d34fb0e6b599169ab289aa465bbf9ce60c45
Mithuna K N <136732204+MithunaKN@users.noreply.github.com>
2023-09-13 11:44:28 +0530

    diabetes.py

--- diabetes.py
+++ diabetes.py
@@ -6,20 +6,24 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import OrdinalEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
+
 # SETTING PAGE CONFIGURATIONS
 st.set_page_config(page_title="Diabetes Check",
                    layout="wide",
                    initial_sidebar_state="expanded"
                    )
 st.markdown("<h1 style='text-align: center; color: #900C3F ;'>Diabetes Check</h1>", unsafe_allow_html=True)
+
 # CREATING TABS
 tab1, tab2 = st.tabs(["Home","Upload and Check"])
+
 # Home menu
 with tab1:
     st.markdown("## **TECHNOLOGIES USED :** Python, Streamlit, ML Algorithm")
-    st.markdown("## **OVERVIEW :** In this streamlit web app, you can upload your necessary details regarding your health conditions")
-    st.markdown("## • You can check whether you have diabetes or not.")
-    st.markdown("## • You can also check how accurate the prediction is done.")
+    st.markdown("## Hey guy, Are you scared to go to the doctor to check your diabetes condition?")
+    st.markdown("##### You don't have to worry. This app would be helpful to check your diabetes without consulting the doctor.")
+    st.markdown("##### You just have to enter certain details that are asked regarding your health conditions and your results will be given in no time.")
+
 # Upload and Check
 with tab2:
     st.markdown("### Upload your details below")
@@ -32,22 +36,32 @@ with tab2:
     bmi = st.text_input('Enter you bmi')
     HbA1c_level	= st.text_input('Enter your HbA1c level')
     blood_glucose_level = st.text_input('Enter your blood glucose level')
+    
     if st.button('submit'):
         df = pd.read_csv('diabetes_prediction_dataset.csv')
+        
         # PREPROCESSING
         enc=OrdinalEncoder()
         df["smoking_history"]=enc.fit_transform(df[["smoking_history"]])
         df["gender"]=enc.fit_transform(df[["gender"]])
+        
         # INDEPENDENT AND DEPENDENT VARIABLES
         x= df.drop("diabetes",axis=1)
         y=df["diabetes"]
+        
         # SPLITTING TRAINING AND TESTING DATA
         x_train , x_test , y_train, y_test = train_test_split(x,y,test_size=0.3)
+        
         # TRAINING THE MODEL
         model = DecisionTreeClassifier().fit(x_train,y_train)
         details = np.array([[gender,age,hypertension,heart_disease,smoking_history,bmi,HbA1c_level,blood_glucose_level]])
         y_pred = model.predict(details)
-        st.metric(label='Your result',value=y_pred)
+        
+        if y_pred == 0:
+            st.metric(label='Your result',value="No diabetes(enjoy your sweet time)")
+        else: 
+            st.metric(label='Your result',value="You have diabetes(consult with a doctor)")
+        
         y_pred1 = model.predict(x_test)
         accuracy = accuracy_score(y_test,y_pred1)
         percentage = accuracy*100
